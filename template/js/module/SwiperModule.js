78eb23c806a99aaaa8af760dcba7bfee038f4197
sinhvusinh1997 <sinhvusinh1997@gmail.com>
2022-06-29 09:57:57 +0700

    add course-page

--- template/js/module/SwiperModule.js
+++ template/js/module/SwiperModule.js
@@ -1,7 +1,5 @@
 function swiperCourseHomePage() {
-
-    const courseElem = document.querySelector(".main .course");
-
+    const courseElem = document.querySelector(".main.home-page .course");
     if (courseElem) {
         courseElem.querySelector(".course-inner").classList.add("swiper");
         courseElem.querySelector(".course-list").classList.remove("d-flex");
@@ -10,7 +8,7 @@ function swiperCourseHomePage() {
             item.classList.remove("d-item");
             item.classList.add("swiper-slide");
         });
-    
+
         const courseSwiper = new Swiper(".main .course .swiper", {
             slidesPerView: 1,
             spaceBetween: 30,
@@ -27,19 +25,7 @@ function swiperCourseHomePage() {
     }
 }
 
-export default function SwiperModule() {
-    const bannerElem = document.querySelector(".main .banner");
-    if (bannerElem) {
-        const bannerSwiper = new Swiper(bannerElem, {
-            // autoplay: {
-            //     delay: 2500,
-            //     disableOnInteraction: false,
-            // }
-        });
-    }
-
-
-
+function swiperStudentHomePage() {
     const studentElem = document.querySelector(".main .student .swiper");
     if (studentElem) {
         const stuSwiper = new Swiper(studentElem, {
@@ -69,6 +55,57 @@ export default function SwiperModule() {
             },
         })
     }
+}
+
+function swiperBanner() {
+    const bannerElem = document.querySelector(".main .banner");
+    if (bannerElem) {
+        const bannerSwiper = new Swiper(bannerElem, {
+            // autoplay: {
+            //     delay: 2500,
+            //     disableOnInteraction: false,
+            // }
+        });
+    }
+}
+
+function swiperCouseCousePage() {
+    const courseElem = document.querySelector(".main.couse-page .course");
+
+    console.log(courseElem)
+
+    if (courseElem) {
+        const swiperCouse = new Swiper(".main.couse-page .course .swiper", {
+            slidesPerView: 1,
+            grid: {
+                rows: 2,
+            },
+            spaceBetween: 0,
+            freeMode: true,
+            pagination: {
+                el: ".ctrl-button .swiper-pagination",
+                clickable: true,
+                renderBullet: function (index, className) {
+                    return '<span class="' + className + '">' + (index + 1) + "</span>";
+                },
+            },
+            navigation: {
+                nextEl: ".ctrl-button .swiper-button-next",
+                prevEl: ".ctrl-button .swiper-button-prev",
+            },
+            breakpoints: {
+                580: {
+                    slidesPerView: 2,
+                    spaceBetween: 30
+                }
+            }
+        })
+    }
+}
+export default function SwiperModule() {
+    swiperBanner();
+    swiperStudentHomePage();
+    swiperCouseCousePage();
 
     if (window.innerWidth <= 767) {
         swiperCourseHomePage();
